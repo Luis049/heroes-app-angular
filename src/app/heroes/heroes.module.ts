88dda9a6bc33969bc351a1a2f80f078a748bac99
Luis <ariza.9485@gmail.com>
2023-07-18 00:38:30 -0500

    detalles material angular

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
@@ -9,6 +10,8 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipe/hero-image.pipe';
 
 
 @NgModule({
@@ -18,9 +21,14 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
+
+    //Pipes
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     HeroesRoutingModule,
     MaterialModule,
   ]
